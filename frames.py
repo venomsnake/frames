0ee22d17265fe98485b95fafea32a1e1cf1dbb65
venomsnake <thakurmark07@gmail.com>
2021-02-24 18:56:54 +0530

    UPDATE : Some random fixes and typo

--- frames.py
+++ frames.py
@@ -1,21 +1,22 @@
 import cv2
 import os
 
-vidcap =cv2.VideoCapture('video.mp4')
+#Takes video
+vidcap =cv2.VideoCapture(input("Enter video name: ")) 
 success,image = vidcap.read()
 
-choice = int(input(" 1 for extracting frame every one seond and 2 to extract all frames : "))
+choice = int(input("1 Each frame after one second and 2 All frames : "))
 
 if choice == 1 :
-    pass
     count = 0
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) # extract frame every one second, remove this line if you want to extract every frame
-        cv2.imwrite("frame%d.jpg"% count,image)
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) # extract frame every one second
+        cv2.imwrite("frame%d.jpg"% count,image) # Save frame as JPEG file
         success,image = vidcap.read()
-        print("Read a new frame",success)
+        print("Read a new frame: ",success)
         count +=1
-else:
+        
+elif choice == 2:
     print("Extracting each frame")
     count = 0
     while success:
@@ -23,7 +24,13 @@ else:
       success,image = vidcap.read()
       print('Read a new frame: ', success)
       count += 1
-      
-print("Cleaning the terminal")
 
-os.system("cls")
+    print("Cleaning the Terminal")
+    os.system("cls")
+      
+else:
+    print("Closing the Terminal")
+    for i in range (1,6):
+        print("Closing in" , i , "seconds")
+        os.system("exit()")
+    
