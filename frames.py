54b8ca62934e667a5049e7c0a0ddf0e57cad0076
venomsnake <thakurmark07@gmail.com>
2021-02-24 15:43:04 +0530

    UPDATE:Extract every second frame

--- frames.py
+++ frames.py
@@ -1,8 +1,11 @@
 import cv2
+import argparse
+
 vidcap =cv2.VideoCapture('video.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) # extract frame every one second, remove this line if you want to extract every frame
     cv2.imwrite("frame%d.jpg"% count,image)
     success,image = vidcap.read()
     print("Read a new frame",success)
